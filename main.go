4ca523298f795b915316238219ad618737b3c520
Nah0mm <nahom.muller@yahoo.com>
2026-05-06 10:09:44 +0300

    /status api added

--- main.go
+++ main.go
@@ -2,35 +2,14 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"system-reporter/converter"
-	"system-reporter/notification"
-	"time"
+	"net/http"
+	"system-reporter/getstatus"
 )
 
 func main() {
-	startTime := time.Now()
-	devices, err := converter.Converter()
-	ereceiver := notification.Email{Address: "someone@gmail.com"}
-	smsreceiver := notification.SMS{PhoneNumber: "25187456987"}
-	if err != nil {
-		fmt.Println("Error occcured", err)
-		return
-	}
-	var wg sync.WaitGroup
-	results := make(chan string)
-	for _, device := range devices {
-		wg.Add(1)
-		go (&device).PingDevice(results, &wg)
-	}
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-	for info := range results {
-		fmt.Println(info)
-		notification.ExecuteSend(ereceiver)
-		notification.ExecuteSend(smsreceiver)
-	}
-	fmt.Println("Scan finished in ", time.Since(startTime))
+
+	http.HandleFunc("/status", getstatus.GetStatusHandler)
+	//Start the server on port 9090
+	fmt.Println("Starting the server on port :9090")
+	http.ListenAndServe(":9090", nil)
 }
